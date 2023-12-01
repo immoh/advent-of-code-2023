a971f48b7c353f7372840aabcd420e8dc3636ac1
Immo Heikkinen <immo@previ.se>
2023-12-01 07:50:17 +0200

    Day 1

--- src/day01.clj
+++ src/day01.clj
@@ -1,1 +1,47 @@
-(ns day01)
+(ns day01
+  (:require
+   [clojure.string :as str]))
+
+(defn parse-input [input]
+  (str/split-lines input))
+
+;; Part 1
+
+(defn combine-digits [digits]
+  (+ (* 10 (first digits)) (last digits)))
+
+(defn calibration-value [line]
+  (->> line
+       (map str)
+       (keep parse-long)
+       (combine-digits)))
+
+(defn part1 [input]
+  (->> input
+       (parse-input)
+       (map calibration-value)
+       (reduce +)))
+
+;; Part 2
+
+(def written-digits (zipmap ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"]
+                           (range)))
+
+(defn digits [line]
+  (keep (fn [i]
+          (or (parse-long (subs line i (inc i)))
+              (get written-digits (subs line i (min (+ i 3) (count line))))
+              (get written-digits (subs line i (min (+ i 4) (count line))))
+              (get written-digits (subs line i (min (+ i 5) (count line))))))
+        (range (count line))))
+
+(defn calibration-value2 [line]
+  (-> line
+      digits
+      combine-digits))
+
+(defn part2 [input]
+  (->> input
+       (parse-input)
+       (map calibration-value2)
+       (reduce +)))
