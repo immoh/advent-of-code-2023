45a83e9b9ead479309f501a89dce4ceb0b692339
Immo Heikkinen <immo.heikkinen@gmail.com>
2023-12-18 21:10:54 +0200

    Day 18, part 2

--- src/day18.clj
+++ src/day18.clj
@@ -4,8 +4,8 @@
 
 (defn parse-input [input]
   (map (fn [line]
-         (let [[dir n] (str/split line #" ")]
-           [dir (parse-long n)]))
+         (let [[_ dir n color] (re-find #"(.) (\d+) \(#(.{6})\)" line)]
+           [dir (parse-long n) color]))
        (str/split-lines input)))
 
 (def dirs
@@ -14,74 +14,55 @@
    "L" [0 -1]
    "R" [0 1]})
 
-(defn dig-instruction [{:keys [pos tiles]} [dir n]]
-  (let [new-tiles (->> pos
-                       (iterate #(mapv + % (dirs dir)))
-                       (rest)
-                       (take n))]
-    {:pos (last new-tiles)
-     :tiles (into tiles new-tiles)}))
+(defn vertices [instructions]
+  (reductions (fn [pos [dir n]]
+                (mapv + pos (map (partial * n) (dirs dir))))
+              [0 0]
+              instructions))
 
-(defn dig-trenches [plan]
-  (reduce dig-instruction
-          {:pos [0 0]
-           :tiles #{}}
-          plan))
+;; Shoelace theorem
+(defn inner-area [instructions]
+  (let [vertices (vertices instructions)]
+    (-> (- (->> vertices
+                (partition 2 1)
+                (map (fn [[[x1 _] [_ y2]]] (* x1 y2)))
+                (reduce +))
+           (->> vertices
+                (partition 2 1)
+                (map (fn [[[_ y1] [x2 _]]] (* y1 x2)))
+                (reduce +)))
+        (/ 2)
+        (abs))))
 
-(defn partition-continuous [row]
-  (let [{:keys [current groups]} (reduce (fn [{:keys [current groups]} [_ y :as tile]]
-                                           (let [[_ ly] (last current)]
-                                             (if (or (not (seq current)) (= y (inc ly)))
-                                               {:current (conj current tile)
-                                                :groups  groups}
-                                               {:current [tile]
-                                                :groups  (conj groups current)})))
-                                         {:current []
-                                          :groups  []}
-                                         row)]
-    (conj groups current)))
+(defn border-area [instructions]
+  (-> (->> instructions
+           (map second)
+           (reduce +))
+      (/ 2)
+      (inc)))
 
-(defn horizontal-border-groups [tiles]
-  (let [min-x (reduce min (map first tiles))
-        max-x (reduce max (map first tiles))]
-    (mapcat (fn [x]
-              (->> tiles
-                   (filter (fn [[x' _]] (= x x')))
-                   (sort)
-                   (partition-continuous)))
-            (range min-x (inc max-x)))))
+(defn calculate-area [instructions]
+  (+ (inner-area instructions)
+     (border-area instructions)))
 
-(defn crossing? [tiles border-group]
-  (or (= 1 (count border-group))
-      (let [[fx fy] (first border-group)
-            [lx ly] (last border-group)]
-        (or (and (tiles [(inc fx) fy]) (tiles [(dec lx) ly]))
-            (and (tiles [(dec fx) fy]) (tiles [(inc lx) ly]))))))
-
-
-(defn inside? [tiles border-groups [x y]]
-  (or (tiles [x y])
-      (odd? (->> border-groups
-                 (filter (fn [[[fx fy]]]
-                           (and (= x fx)
-                                (< y fy))))
-                 (filter (partial crossing? tiles))
-                 (count)))))
-
-(defn inside-tiles [tiles]
-  (let [min-x (reduce min (map first tiles))
-        max-x (reduce max (map first tiles))
-        min-y (reduce min (map second tiles))
-        max-y (reduce max (map second tiles))
-        border-groups (horizontal-border-groups tiles)]
-    (->> (for [x (range min-x (inc max-x))
-               y (range min-y (inc max-y))]
-           [x y])
-         (filter (partial inside? tiles border-groups)))))
+;; Part 1
 
 (defn part1 [input]
   (->> (parse-input input)
-       (dig-trenches)
-       :tiles
-       (inside-tiles)
-       (count)))
+       (calculate-area)))
+
+;; Part 2
+
+(defn fix-instruction [[_ _ param]]
+  [({"0" "R"
+     "1" "D"
+     "2" "L"
+     "3" "U"} (subs param 5 6))
+   (Long/valueOf (subs param 0 5) 16)])
+
+(defn part2 [input]
+  (->> (parse-input input)
+       (map fix-instruction)
+       (calculate-area)))
+
+
